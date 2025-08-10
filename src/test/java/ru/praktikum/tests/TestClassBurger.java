586ae3c3c588a45b188f16c62cbbe7ed092e103f
Liubov Butorina <liubovbutorina7@yandex.ru>
2025-08-10 14:01:36 +0300

    added testGetPrice

--- src/test/java/ru/praktikum/tests/TestClassBurger.java
+++ src/test/java/ru/praktikum/tests/TestClassBurger.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.*;
@@ -16,6 +17,12 @@ public class TestClassBurger {
     private Burger burger;
     @Mock
     private Bun mockBun;
+    @Mock
+    private Ingredient mockIngredient1;
+    @Mock
+    private Ingredient mockIngredient2;
+    @Mock
+    private Ingredient mockIngredient3;
 
     @Before
     public void setUp() {
@@ -27,4 +34,22 @@ public class TestClassBurger {
         burger.setBuns(mockBun);
         assertEquals(mockBun, burger.bun);
     }
+
+    @Test
+    public void testGetPrice() throws Exception {
+        burger.setBuns(mockBun);
+        burger.addIngredient(mockIngredient1);
+        burger.addIngredient(mockIngredient2);
+        burger.addIngredient(mockIngredient3);
+
+        Mockito.when(mockBun.getPrice()).thenReturn(500f);
+        Mockito.when(mockIngredient1.getPrice()).thenReturn(200f);
+        Mockito.when(mockIngredient2.getPrice()).thenReturn(400f);
+        Mockito.when(mockIngredient3.getPrice()).thenReturn(300f);
+
+        float expectedPrice = 1900;
+        float delta = 0.001f;
+        assertEquals(expectedPrice, burger.getPrice(), delta);
+    }
+
 }
